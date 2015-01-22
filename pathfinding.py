5837636587dd157ffc743a037217a5bef3086759
Julian <julianrgrey@gmail.com>
2015-01-22 21:53:33 +0000

    Point-to-point pathfinding complete

--- pathfinding.py
+++ pathfinding.py
@@ -1,36 +1,82 @@
 class Map():
     list_grid_cells = []
-    num_cols = 8
-    num_rows = 6
+    num_cols = 40
+    num_rows = 40
 
     for y in range(num_rows):
         for x in range(num_cols):
             list_grid_cells.append((x, y))
 
-    def draw_map():
-        pass
+
+def draw_map(selected_map):
+    for cell in selected_map.list_grid_cells:
+        if (cell[0] + 1) % selected_map.num_cols == 0:
+            print 'o'
+        else:
+            print 'o',
 
 
-def find_adjacent_cell(cell):
-    pass
+def find_adjacent_cells(current_cell):
+    cell_x = current_cell[0]
+    cell_y = current_cell[1]
 
+    cell_left = (cell_x - 1, cell_y)
+    cell_right = (cell_x + 1, cell_y)
+    cell_up = (cell_x, cell_y - 1)
+    cell_down = (cell_x, cell_y + 1)
+
+    return [cell_left, cell_right, cell_up, cell_down]
 
 
 def pathfinding():
     my_map = Map()
     list_cells = my_map.list_grid_cells
-    start_cell = (1, 4)
-    destination = (6, 3)
+
+    start_cell = (36, 24)
+    destination = (5, 36)
+
     open_cells = []
     closed_cells = []
+    adj_cells = []
+    current_cell = None
 
-    if start_cell in list_cells:
-        list_cells.remove(start_cell)
+    open_cells.append(start_cell)
+    closed_cells.append(start_cell)
+    
+    if current_cell is None:
+        current_cell = closed_cells[-1]
+        open_cells.remove(current_cell)
 
-    # for cell in my_map.list_grid_cells:
-    #     if cell[0] == start_cell[0] and cell[1] == start_cell[1]:
-    #         my_map.list_grid_cells.remove(cell)
-    print my_map.list_grid_cells
+    while current_cell is not destination:
+        adj_cells = find_adjacent_cells(current_cell)
+        open_cells.extend(adj_cells)
+        selection = []
+        next_cell = None
+        next_cell_distance = None
+
+        for cell in adj_cells:
+            if cell not in closed_cells:
+                selection.append(cell)
+        
+        for cell in selection:
+            distance = abs(destination[0] - cell[0]) + \
+                       abs(destination[1] - cell[1])
+            if next_cell is None:
+                next_cell = cell
+                next_cell_distance = distance
+            else:
+                if distance < next_cell_distance:
+                    next_cell = cell
+                    next_cell_distance = distance
+
+        current_cell = next_cell
+        closed_cells.append(current_cell)
+        open_cells.remove(current_cell)
+
+        if current_cell == destination:
+            break
+
+    print closed_cells
 
 
 pathfinding()

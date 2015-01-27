bd306af3a87d46a6ed4b66ac75032fb0f186483d
Julian <julianrgrey@gmail.com>
2015-01-27 19:48:28 +0000

    Change tests to reflect the tuple to list change

--- tests.py
+++ tests.py
@@ -21,11 +21,11 @@ class TestMap(unittest.TestCase):
                          len(self.test_map.build_map()))
 
     def test_cell_exists(self):
-        cell = (5, 5, True)
+        cell = [5, 5, True]
         self.assertIn(cell, self.test_map.build_map())
 
     def test_cell_does_not_exist(self):
-        cell = (11, 3, True)
+        cell = [11, 3, True]
         self.assertNotIn(cell, self.test_map.build_map())
 
 
@@ -35,11 +35,11 @@ class TestPathfinding(unittest.TestCase):
         self.test_map = MapNoObstacles(10, 10)
 
     def test_adjacent_cells(self):
-        cell = (5, 5, True)
-        north_cell = (5, 4, True)
-        east_cell = (6, 5, True)
-        south_cell = (5, 6, True)
-        west_cell = (4, 5, True)
+        cell = [5, 5, True]
+        north_cell = [5, 4, True]
+        east_cell = [6, 5, True]
+        south_cell = [5, 6, True]
+        west_cell = [4, 5, True]
         self.assertIn(north_cell,
                       find_adjacent_cells(cell, self.test_map.build_map()))
         self.assertIn(east_cell,

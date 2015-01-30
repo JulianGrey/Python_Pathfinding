31776dfd0ef0979f4dc8ecaddb93eccac2f368d0
Julian <julianrgrey@gmail.com>
2015-01-30 11:14:54 +0000

    Rename variable to be more descriptive

--- adjacency.py
+++ adjacency.py
@@ -10,7 +10,7 @@ def find_adjacent_cells(current_cell, grid, distance_moved):
     cells.append([cell_x, cell_y + 1, True, distance_moved + 1, 0, None])
 
     for cell in cells:
-        cell_base = [cell[0], cell[1], cell[2], 0, cell[4], cell[5]]
-        if cell_base in grid:
+        cell_no_move_cost = [cell[0], cell[1], cell[2], 0, cell[4], cell[5]]
+        if cell_no_move_cost in grid:
             adj_cells.append(cell)
     return adj_cells
